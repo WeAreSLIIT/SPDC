637c0046d1ad06772372b78022031fd63d2ab4bb
Samith Dilshan <samithdilsh@gmail.com>
2018-09-21 21:19:12 +0530

    Login finilize

--- BackEnd/routes/users.js
+++ BackEnd/routes/users.js
@@ -1,7 +1,27 @@
 const express= require('express');
 const router = express.Router();
 const User = require('../models/user');
+const bcryptjs = require('bcryptjs');
 
+router.post("/login",(req,res)=>{
+    User.findOne({username:req.body.username}).then((user)=>{
+        bcryptjs.compare(req.body.password,user.password,(err,isMatch)=>{
+            if(err) throw err;
+            if(isMatch) {
+                res.json({
+                    user,
+                    login :'success'
+                });
+            }
+            else {
+                res.json({
+                    login:'fail'
+                });
+            }
+        });
+    });
+   
+});
 router.get("/",(req,res)=>{
     User.find({}).then((users)=>{
         res.send(users);
@@ -15,10 +35,14 @@ router.get("/:username",(req,res)=>{
 });
 
 router.post("/",(req,res)=>{
-    User.create(req.body).then((user)=>{
-        res.send(user);
-    });
-   
+   bcryptjs.genSalt(10,(err,salt)=>{
+       bcryptjs.hash(req.body.password,salt,(err,hash)=>{
+           req.body.password = hash;
+           User.create(req.body).then((user)=>{
+            res.send(user);
+        });
+       });
+   });
 });
 
 router.delete("/:username",(req,res)=>{
